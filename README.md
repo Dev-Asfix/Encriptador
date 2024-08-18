b51f5d30fcbf76e61a58797c21260f6f481b0e45
Dev-Asfix <rosasramirez753@gmail.com>
2024-08-18 09:36:33 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# 🌟 **Encriptador de Texto Futurista** 🌟
+
+Desarrollado por **Pablo Aldair Rosas Ramírez** para el **Challenge de Alura** en la **Beca de Oracle**.
+
+---
+
+## 📜 **Introducción**
+
+Este proyecto combina un diseño moderno con una funcionalidad eficiente para encriptar y desencriptar mensajes. 
+
+---
+
+## 🚀 **Vista General**
+
+![Encriptador de Texto](/capturas/Captura1.png)  
+*Diseño Futurista del Encriptador*
+
+![Encriptador de Texto](/capturas/Captura2.png)  
+*Diseño Futurista del Encriptador*
+
+### **Características Principales**
+
+- **Interfaz Interactiva:** Estilo oscuro con colores cian y magenta.
+- **Funcionalidades:**
+  - Encriptación de texto.
+  - Desencriptación de texto.
+  - Copia al portapapeles.
+  - Validación de entrada para mayor precisión.
+  - No se puede escribir tildes , ni mayúculas 
+
+---
---
+++ script.js
@@ -0,0 +1,78 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+/* La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat" */
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";mensaje.style.backgroundImage = "none";
+}
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e", "enter"], ["i","imes"], ["a", "ai"], ["o","ober"], ["u","ufat"]];
+    console.table(matrizCodigo)
+    stringEncriptada = stringEncriptada.toLowerCase()
+
+    for(let i=0; i< matrizCodigo.length;i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada= stringEncriptada.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptada
+
+}
+
+
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";mensaje.style.backgroundImage = "none";
+   
+}
+
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["e", "enter"], ["i","imes"], ["a", "ai"], ["o","ober"], ["u","ufat"]];
+    console.table(matrizCodigo)
+    stringDesencriptada = stringDesencriptada.toLowerCase()
+
+    for(let i=0; i< matrizCodigo.length;i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada= stringDesencriptada.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0])
+        }
+    }
+    return stringDesencriptada
+
+}
+
+async function CopiarTexto(){
+    try{
+        const textAreaCopy = document.getElementById('mensaje-copiar');
+        const texto = textAreaCopy.value;
+
+        await navigator.clipboard.writeText(texto);
+
+        alert('Copiado Correctamente.')
+    } catch(err){
+        console.error('Error al copiar al portapapeles: ', err)
+    }
+}
+
+textArea.addEventListener("keypress", function(event) {
+    const key = event.key;
+    const acentos = ['á', 'é', 'í', 'ó', 'ú', 'Á', 'É', 'Í', 'Ó', 'Ú'];
+
+    if (acentos.includes(key)) {
+        event.preventDefault();
+    }
+
+    if (key === key.toUpperCase() && key !== key.toLowerCase()) {
+        event.preventDefault();
+        textArea.value += key.toLowerCase(); // Añadir la versión en minúscula
+    }
+
+});
